d5447037ba4dc881c03d38b80ed8a028e67d209a
FrankMa <FMa@bauermedia.co.nz>
2016-05-19 14:22:15 +1200

    get application id

--- start.js
+++ start.js
@@ -7,7 +7,37 @@ var driver = new webdriver.Builder()
     .build();
 
 driver.get('https://onlineservices.immigration.govt.nz/secure/Login+Working+Holiday.htm');
-driver.findElement(By.id('OnlineServicesLoginStealth_VisaLoginControl_userNameTextBox'));
+driver.wait(function() {
+    return driver.isElementPresent(By.id('OnlineServicesLoginStealth_VisaLoginControl_userNameTextBox'));
+}, 3000);
+driver.findElement(By.id('OnlineServicesLoginStealth_VisaLoginControl_userNameTextBox')).sendKeys("pipilu");
+driver.findElement(By.id('OnlineServicesLoginStealth_VisaLoginControl_passwordTextBox')).sendKeys("MY2014code");
+driver.findElement(By.id('OnlineServicesLoginStealth_VisaLoginControl_loginImageButton')).click();
+var grabApplicationId = function() {
+    driver.get('https://onlineservices.immigration.govt.nz/WORKINGHOLIDAY/Application/Create.aspx?CountryId=46');
+    driver.wait(function() {
+        return driver.isElementPresent(By.id('ctl00_ContentPlaceHolder1_whsStatusPanel'));
+    }, 15000);
+    driver.findElement(By.id('ctl00_ContentPlaceHolder1_applyNowButton')).then(
+        function(applyButton){
+            driver.findElement(By.id('ctl00_ContentPlaceHolder1_countryLabel')).getText().then(function(countryText) {
+                if (countryText == 'China') {
+                    applyButton.click();
+                    driver.getCurrentUrl().then(function(currentUrl) {
+                        console.log(currentUrl);
+                    });
+                } else {
+                    console.log('Not scheme for China');
+                }
+            });
+        },
+        function() {
+            grabApplicationId();
+        }
+    );
+};
+grabApplicationId();
+
 // driver.findElement(By.name('btnG')).click();
 // driver.wait(until.titleIs('webdriver - Google Search'), 1000);
 // driver.quit();
