24a548b6a66e2423b29aa8351f1eb689c9d380ca
FrankMa <semstar2010@gmail.com>
2016-05-22 00:26:30 +1200

    control start stop and added users

--- start.js
+++ start.js
@@ -5,23 +5,48 @@ var By = require('selenium-webdriver').By;
 
 var service = new chrome.ServiceBuilder(path).build();
 chrome.setDefaultService(service);
-
 var driver = new webdriver.Builder()
     .withCapabilities(webdriver.Capabilities.chrome())
     .build();
 
-driver.get('https://onlineservices.immigration.govt.nz/secure/Login+Working+Holiday.htm');
-driver.wait(function() {
-    return driver.isElementPresent(By.id('OnlineServicesLoginStealth_VisaLoginControl_userNameTextBox'));
-}, 3000);
-driver.findElement(By.id('OnlineServicesLoginStealth_VisaLoginControl_userNameTextBox')).sendKeys("pipilu");
-driver.findElement(By.id('OnlineServicesLoginStealth_VisaLoginControl_passwordTextBox')).sendKeys("MY2014code");
-driver.findElement(By.id('OnlineServicesLoginStealth_VisaLoginControl_loginImageButton')).click();
-var grabApplicationId = function() {
-    driver.get('https://onlineservices.immigration.govt.nz/WORKINGHOLIDAY/Application/Create.aspx?CountryId=46');
+var users = {
+    user0: {name: 'bumei', password: 'MY2016code'},
+    user1: {name: 'boniu', password: 'MY2016code'},
+    user2: {name: 'zongjie', password: 'MY2016code'},
+    user3: {name: 'huiyuanai', password: 'MY2016code'},
+    test1: {name: 'pipinu', password: 'MY2014code'},
+    test2: {name: 'yuyuyuyu', password: 'MY2012code'}
+};
+var user = null;
+var stopFlag = false;
+
+var login = function(userLabel) {
+    user = users[userLabel];
+    if (user == undefined) {
+        console.log('No such user');
+        return;
+    }
+    driver.get('https://onlineservices.immigration.govt.nz/secure/Login+Working+Holiday.htm');
     driver.wait(function() {
-        return driver.isElementPresent(By.id('ctl00_ContentPlaceHolder1_whsStatusPanel'));
-    }, 15000);
+        return driver.isElementPresent(By.id('OnlineServicesLoginStealth_VisaLoginControl_userNameTextBox'));
+    }, 10000);
+    driver.findElement(By.id('OnlineServicesLoginStealth_VisaLoginControl_userNameTextBox')).sendKeys(user.name);
+    driver.findElement(By.id('OnlineServicesLoginStealth_VisaLoginControl_passwordTextBox')).sendKeys(user.password);
+    driver.findElement(By.id('OnlineServicesLoginStealth_VisaLoginControl_loginImageButton')).click();
+};
+var grabApplicationId = function() {
+    if (stopFlag) {
+        return;
+    }
+    driver.get('https://onlineservices.immigration.govt.nz/WORKINGHOLIDAY/Application/Create.aspx?CountryId=46');
+    driver.wait(
+        function() {
+            return driver.isElementPresent(By.id('ctl00_ContentPlaceHolder1_whsStatusPanel'));
+        },
+        20000
+    ).then(function(){}, function() {
+        console.log('Scheme page not right.');
+    });
     driver.findElement(By.id('ctl00_ContentPlaceHolder1_applyNowButton')).then(
         function(applyButton){
             driver.findElement(By.id('ctl00_ContentPlaceHolder1_countryLabel')).getText().then(function(countryText) {
@@ -29,6 +54,7 @@ var grabApplicationId = function() {
                     applyButton.click();
                     driver.getCurrentUrl().then(function(currentUrl) {
                         console.log(currentUrl);
+                        console.log(user.name);
                     });
                 } else {
                     console.log('Not scheme for China');
@@ -40,7 +66,38 @@ var grabApplicationId = function() {
         }
     );
 };
-grabApplicationId();
+var handleInput = function(input) {
+    switch (input.command) {
+        case 'login':
+            login(input.args);
+            break;
+        case 'start':
+            stopFlag = false;
+            grabApplicationId();
+            break;
+        case 'stop':
+            stopFlag = true;
+            break;
+        default:
+            console.log('Input not recognised.');
+    }
+};
+
+var parseInput = function(input) {
+    var matches = input.toString().match(/(\w+)(.*)/);
+    return {
+        command: matches[1].toLowerCase(),
+        args: matches[2].trim()
+    };
+};
+
+process.stdin.on('data', function(input) {
+    try {
+        handleInput(parseInput(input));
+    } catch (e) {
+        console.log(e);
+    }
+});
 
 // driver.findElement(By.name('btnG')).click();
 // driver.wait(until.titleIs('webdriver - Google Search'), 1000);
